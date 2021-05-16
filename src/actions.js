2a6cdddc52a6a85bc65494b39339edff3a9f3bd5
mrafiq709 <mrafiq709@gmail.com>
2021-05-16 13:09:12 +0700

    initial commit

---
+++ src/actions.js
@@ -0,0 +1,32 @@
+import * as actions from "./actionTypes";
+
+export const bugAdded = (description) => ({
+  type: actions.BUG_ADDED,
+  payload: {
+    description,
+  },
+});
+// export function bugAdded(description) {
+//   return {
+//     type: actions.BUG_ADDED,
+//     payload: {
+//       description,
+//     },
+//   };
+// }
+
+export function bugRemoved(id) {
+  return {
+    type: actions.BUG_REMOVED,
+    payload: {
+      id,
+    },
+  };
+}
+
+export const bugResolved = (id) => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id,
+  },
+});
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
+store.dispatch(bugAdded("Bug 1"));
+
+unsubscribe();
+
+// store.dispatch(bugRemoved(1));
+
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
