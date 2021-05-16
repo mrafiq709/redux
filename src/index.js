6eac50ee2008f389ced7d91fdcec73b2fdc1f3a5
mrafiq709 <mrafiq709@gmail.com>
2021-05-16 14:44:22 +0700

    Building Redux From Scracth

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import store from "./customStore";
+import * as actions from "./actions";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed!", store.getState());
+store.subscribe(() => {
+  console.log("Store changed!");
 });
-
-store.dispatch(bugAdded("Bug 1"));
-
-unsubscribe();
-
-// store.dispatch(bugRemoved(1));
-
-store.dispatch(bugResolved(1));
+store.dispatch(actions.bugAdded("Bug 1"));
 
 console.log(store.getState());
