aa4e98e9d78c27d770b5ac3d623faae2bdc4a457
mrafiq709 <mrafiq709@gmail.com>
2021-05-16 20:51:00 +0700

    clean redux code, using @reduxjs/toolkit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import store from "./store";
-import * as actions from "./actions";
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
+
+import { projectAdded } from "./store/projects";
+
+const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store changed!");
 });
-store.dispatch(actions.bugAdded("Bug 1"));
-store.dispatch(actions.bugAdded("Bug 2"));
-store.dispatch(actions.bugAdded("Bug 3"));
-store.dispatch(actions.bugResolved(1));
+
+store.dispatch(projectAdded({ name: "Project 1" }));
+
+store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 console.log(store.getState());
---
+++ src/store/bugs.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let lastid = 0;
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    // actions => action handlers
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastid,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+  },
+});
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
